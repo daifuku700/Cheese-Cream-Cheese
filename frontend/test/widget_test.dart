96d56219389edb2e39034ae27bb1bf98b83da8cc
pentabi <tabitosatoh@gmail.com>
2024-01-28 18:40:25 +0900

    間違えてmainで作業してたごめん

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cream_cheese_cream/main.dart';
+import 'package:cream_cheese_cream/homepage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
