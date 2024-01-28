85e247576a98e21182e4d763a0230f9441cfb528
pentabi <tabitosatoh@gmail.com>
2024-01-28 19:45:12 +0900

    ページ推移完了

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cream_cheese_cream/homepage.dart';
+import 'package:cream_cheese_cream/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
