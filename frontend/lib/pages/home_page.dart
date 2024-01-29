ee8f882100eb5480c7efd811e2bd165d2446b0c3
pentabi <tabitosatoh@gmail.com>
2024-01-29 23:07:06 +0900

    へい

--- frontend/lib/pages/home_page.dart
+++ frontend/lib/pages/home_page.dart
@@ -12,9 +12,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child:Text("Home Page")
-      ),
+      body: Center(child: Text("Home Page")),
     );
   }
 }
