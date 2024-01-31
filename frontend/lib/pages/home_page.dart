4906d4857648a7cd0dc2fb6e0d8744da24c25e6c
pentabi <tabitosatoh@gmail.com>
2024-01-31 16:43:28 +0900

    home_page

--- frontend/lib/pages/home_page.dart
+++ frontend/lib/pages/home_page.dart
@@ -77,23 +77,75 @@ class _HomePageState extends State<HomePage> {
     return formatter.format(now);
   }
 
+  //天気の画像を変える
+  String getImagePath(String weatherText) {
+    if (weatherText != null && weatherText.isNotEmpty) {
+      switch (weatherText[0]) {
+        case '晴':
+          return "assets/home_page/sunny.png";
+        case '曇':
+          return "assets/home_page/cloudy.png";
+        case '雨':
+          return "assets/home_page/rain.png";
+        case '雪':
+          return "assets/home_page/snow.png";
+        default:
+          return "assets/home_page/sunny_cloudy.png";
+      }
+    } else {
+      return "assets/home_page/sunny_cloudy.png";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     print("hi");
     print(events);
     print(weather);
+
     return Scaffold(
       backgroundColor: Color(0xFFEFF8FF),
       body: Center(
         child: Column(
           children: <Widget>[
             //おはよう今日は〇〇の所
-            Container(
+            Expanded(
+              flex: 1,
+              child: Container(
+                  margin: const EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5), // Set shadow color
+                        spreadRadius: 2, // Set the spread radius of the shadow
+                        blurRadius: 5, // Set the blur radius of the shadow
+                        offset: Offset(0, 3), // Set the offset of the shadow
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Text(
+                      'おはよう！\n今日は${_getFormattedDate()}', // Add your text here
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.normal,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  )),
+            ),
+
+            //今日の予定
+            Expanded(
+              flex: 2,
+              child: Container(
+                padding: const EdgeInsets.all(10.0),
                 margin: const EdgeInsets.all(15.0),
-                height: 70,
                 decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.0),
+                  borderRadius: BorderRadius.circular(15.0),
                   boxShadow: [
                     BoxShadow(
                       color: Colors.grey.withOpacity(0.5), // Set shadow color
@@ -103,190 +155,181 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ],
                 ),
-                child: Center(
-                  child: Text(
-                    'おはよう！\n今日は${_getFormattedDate()}', // Add your text here
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.normal,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                )),
-
-            //今日の予定
-            Container(
-              padding: const EdgeInsets.all(10.0),
-              margin: const EdgeInsets.all(15.0),
-              height: 120,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5), // Set shadow color
-                    spreadRadius: 2, // Set the spread radius of the shadow
-                    blurRadius: 5, // Set the blur radius of the shadow
-                    offset: Offset(0, 3), // Set the offset of the shadow
-                  ),
-                ],
-              ),
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    '今日の予定', // Add your text here
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.normal,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-
-                  //一つ一つの予定
-                  Container(
-                    height: 60,
-                    child: Scrollbar(
-                      child: ListView.builder(
-                        itemCount: events.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            margin: const EdgeInsets.symmetric(
-                                vertical: 5, horizontal: 10),
-                            height: 20,
-                            decoration: BoxDecoration(
-                              color: index % 2 == 0
-                                  ? Color(0xFFA9CF58)
-                                  : Color(0xFF75CDFF),
-                              borderRadius: BorderRadius.circular(20.0),
-                            ),
-                            child: Center(
-                              child: Text(
-                                events[index]['summary'] ?? '予定${index + 1}',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.normal,
-                                ),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                    child: Image.asset(
-                      "assets/home_page/cloudy.png",
-                      width: 50,
-                      height: 50,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                      padding: const EdgeInsets.all(10.0),
-                      margin: const EdgeInsets.all(15.0),
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(15.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 2,
-                            blurRadius: 5,
-                            offset: Offset(0, 3),
-                          ),
-                        ],
-                      ),
-                      child: Center(
-                        child: Text(
-                          '15℃', // Add your text here
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.normal,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      )),
-                ),
-              ],
-            ),
-
-            //持ち物リスト
-            Container(
-              margin: const EdgeInsets.all(15.0),
-              height: 150, // Adjust the height to accommodate the list
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'これ持った？',
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      '今日の予定', // Add your text here
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.normal,
                       ),
                       textAlign: TextAlign.center,
                     ),
+
+                    //一つ一つの予定
+                    Container(
+                      height: 60,
+                      child: Scrollbar(
+                        child: ListView.builder(
+                          itemCount: events.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              margin: const EdgeInsets.symmetric(
+                                  vertical: 5, horizontal: 10),
+                              height: 20,
+                              decoration: BoxDecoration(
+                                color: index % 2 == 0
+                                    ? Color(0xFFA9CF58)
+                                    : Color(0xFF75CDFF),
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  events[index]['summary'] ?? '予定${index + 1}',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      child: Image.asset(
+                        getImagePath(weather["text"]),
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
                   ),
                   Expanded(
-                    child: ListView.builder(
-                      itemCount: events.length,
-                      itemBuilder: (context, index) {
-                        return Column(
-                          children: [
-                            ListTile(
-                              title: Text(
-                                  events[index]['summary'] ?? '予定${index + 1}'),
-                              // Other ListTile properties as needed
+                    child: Container(
+                        padding: const EdgeInsets.all(10.0),
+                        margin: const EdgeInsets.all(15.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset: Offset(0, 3),
                             ),
-                            // Display items for each event
-                            ListView.builder(
-                              shrinkWrap: true,
-                              physics: const ClampingScrollPhysics(),
-                              itemCount: events[index]['items'].length,
-                              itemBuilder: (context, itemIndex) {
-                                final item = events[index]['items'][itemIndex];
-                                return ListTile(
-                                  title: Text(item['name'] ?? ''),
-                                  subtitle: Text(
-                                      'Category: ${item['category'] ?? ''}'),
-                                  // Add other item details as needed
-                                );
-                              },
-                            ),
-                            // Divider between events
-                            const Divider(),
                           ],
-                        );
-                      },
-                    ),
+                        ),
+                        child: Center(
+                            child: Text(
+                          'high: ${weather["temperature_max"] ?? "N/A"}℃\nlow: ${weather["temperature_min"] ?? "N/A"}℃',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                          ),
+                        ))),
                   ),
                 ],
               ),
             ),
-            Text(
-              'いってらっしゃい！',
+
+            //持ち物リスト
+            Expanded(
+              flex: 2,
+              child: Container(
+                margin: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'これ持った？',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Expanded(
+                      //イベントを全て探索
+                      //TODO: 同日のものだけ表示できるようにさせる
+                      child: ListView.builder(
+                        itemCount: events.length,
+                        itemBuilder: (context, index) {
+                          return Column(
+                            children: [
+                              //イベント全てのものを出力する
+                              ListView.builder(
+                                shrinkWrap: true,
+                                physics: const ClampingScrollPhysics(),
+                                itemCount: events[index]['items'].length,
+                                itemBuilder: (context, itemIndex) {
+                                  final item =
+                                      events[index]['items'][itemIndex];
+
+                                  return Row(
+                                    children: [
+                                      if (itemIndex % 2 == 0)
+                                        Container(
+                                          margin: const EdgeInsets.symmetric(
+                                              horizontal: 15.0),
+                                          height: 30,
+                                          color: Colors.blue,
+                                          child: Text(item['name'] ?? ''),
+                                        )
+                                      else
+                                        Container(
+                                          margin:
+                                              const EdgeInsets.only(left: 15.0),
+                                          height: 30,
+                                          color: Colors.green,
+                                          child: Text(item['name'] ?? ''),
+                                        ),
+                                    ],
+                                  );
+                                },
+                              ),
+
+                              // Divider between events
+                              const Divider(),
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 30),
+              child: Text(
+                'いってらっしゃい！',
+              ),
+            )
           ],
         ),
       ),
